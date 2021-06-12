8827bda2fdcc88bc2cd9ddb6de38bfaeeab3c17e
cpan1995 <cpan1995@gmail.com>
2021-06-11 20:16:30 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    const newArray = []
+    for (let i = 0; i < names.length; i++){
+        
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+
+function countDown(n){
+    while(n != -1){
+        console.log(n)
+        n--
+    }
+}
